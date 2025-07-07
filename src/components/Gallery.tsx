94a33f19333b6305e9e123685fa25bafbfcb3152
Abhinavvhh <abhinavksathyann@gmail.com>
2025-07-08 00:10:44 +0530

    modified:   src/components/Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import { Calendar } from 'lucide-react';
-import event1 from '/public/assets/Photos/events1.jpg';
+
 
 const Gallery: React.FC = () => {
   const [ref, inView] = useInView({
@@ -12,7 +12,7 @@ const Gallery: React.FC = () => {
 
   const galleryImages = [
     {
-      src: event1,
+     src: "/assets/photos/events1.jpg",
       alt: "Startup Pitch Competition",
       title: "Annual Startup Pitch Competition 2024",
       date: "March 2024",
