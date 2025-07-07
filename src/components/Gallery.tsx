12ce86790081c61eb8993fb49673ccfc96e2cbd2
Abhinavvhh <abhinavksathyann@gmail.com>
2025-07-07 23:57:16 +0530

    modified:   src/components/Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import { Calendar } from 'lucide-react';
-import event1 from '../assets/Photos/ESUMMIT25-5(1).jpg';
+import event1 from '../assets/Photos/NEC2024.jpg';
 
 const Gallery: React.FC = () => {
   const [ref, inView] = useInView({
